6261ed0a3bb2a361a6ffe1706949dfff36b61957
mihribanceylan <114102021+mihribanceylan@users.noreply.github.com>
2026-01-23 23:39:59 +0100

    Update pricing_time_changed_SDE.py

--- pricing_time_changed_SDE.py
+++ pricing_time_changed_SDE.py
@@ -148,41 +148,6 @@ def build_augmented_from(S_time_like):
             S_aug[p,t] = np.hstack((S_time_like[p,t,0], X[p,t], QV_reduced[p,t]))
     return S_aug
 
-
-def slice_single_asset_stream(S_time_like, j):
-    """
-    Slices the stream data for a single asset.
-
-    Parameters:
-    - S_time_like: Time-stamped price data
-    - j: Index of the asset
-
-    Returns:
-    - S_time_like sliced for the j-th asset
-    """
-    return S_time_like[:, :, [0, 1 + j]]
-
-def slice_assets_stream(S_time_like, idx_list):
-    """
-    Slices the stream data for multiple assets.
-
-    Parameters:
-    - S_time_like: Time-stamped price data
-    - idx_list: List of asset indices
-
-    Returns:
-    - S_time_like sliced for the specified assets
-    """
-    cols = [0] + [1 + j for j in idx_list]
-    return S_time_like[:, :, cols]
-
-
-
-   
-    
-
-
-
 # ==============================
 # Ridge 
 # ==============================
